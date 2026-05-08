9965a9e9af540c5b53a2141bdab5049fd6c5cd56
DragonCat4012 <>
2026-05-08 12:51:15 +0200

    Wip4

--- main_node/stop_condition/bad_configuration_based.py
+++ main_node/stop_condition/bad_configuration_based.py
@@ -16,7 +16,6 @@ class BadConfigurationBasedType(StopCondition):
         self.logger.debug(f"Currently {bad_configurations_number} bad Configurations in Experiment.")
 
 class BadConfigurationBasedTypeMock(BadConfigurationBasedType):
-    super().__init__(stop_condition_parameters, experiment_description, experiment_id)
-    #self.threshold = stop_condition_parameters["Parameters"]["MaxBadConfigurations"]
-
-    self.start_threads()
+    def __init__(self, stop_condition_parameters: dict, experiment_description: dict, experiment_id: str):
+        super().__init__(stop_condition_parameters, experiment_description, experiment_id)
+        self.start_threads()
